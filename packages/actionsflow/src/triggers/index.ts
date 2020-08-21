08fdafd4a79017410837bd7cf8faa5bb84690709
Owen Young <theowenyoung@gmail.com>
2020-08-21 16:46:15 +0800

    chore: manual update

--- packages/actionsflow/src/triggers/index.ts
+++ packages/actionsflow/src/triggers/index.ts
@@ -1,6 +1,6 @@
-import Rss from "./Rss";
-import Poll from "./Poll";
-import Webhook from "./Webhook";
+import Rss from "./rss";
+import Poll from "./poll";
+import Webhook from "./webhook";
 import TelegramBot from "./telegram-bot";
 
 export { Rss, Poll, Webhook, TelegramBot };
