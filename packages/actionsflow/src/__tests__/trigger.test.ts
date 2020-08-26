0d8963b38b02d87acc2ff3d3dfaa78bfdf84be7d
Owen Young <theowenyoung@gmail.com>
2020-08-27 05:06:23 +0800

    chore: manual update

--- packages/actionsflow/src/__tests__/trigger.test.ts
+++ packages/actionsflow/src/__tests__/trigger.test.ts
@@ -23,28 +23,3 @@ test("run trigger", async () => {
     await result.helpers.cache.reset();
   }
 });
-
-test("run trigger without force update", async () => {
-  process.env.DEFAULT_LOG_LEVEL = "debug";
-
-  const result = await run({
-    trigger: {
-      name: "rss",
-      options: {
-        url: "https://hnrss.org/newest?points=300",
-      },
-      workflowRelativePath: "rss.yml",
-    },
-    context: {
-      github: {
-        event: {},
-      },
-      secrets: {},
-    },
-  });
-  expect(result.items.length).toBe(0);
-  // clear cache
-  if (result.helpers && result.helpers.cache) {
-    await result.helpers.cache.reset();
-  }
-});
