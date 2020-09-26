0350907bc19a804c29772f01ccc7e8f14b93b739
Owen Young <theowenyoung@gmail.com>
2020-09-26 22:35:31 +0800

    fix: typeform

---
+++ packages/actionsflow/src/triggers/__tests__/trigger-exist.test.ts
@@ -0,0 +1,28 @@
+import triggers from "../index";
+import { ITriggerClassTypeConstructable } from "actionsflow-interface";
+import { getTriggerConstructorParams } from "./trigger.util";
+
+test("is all trigger is a valid class", async () => {
+  const allTriggerKeys = Object.keys(triggers);
+  const allTriggers = triggers as Record<
+    string,
+    ITriggerClassTypeConstructable
+  >;
+  for (let i = 0; i < allTriggerKeys.length; i++) {
+    const key = allTriggerKeys[i];
+    console.log("key", key);
+
+    const Trigger = allTriggers[key];
+    console.log("Trigger", Trigger);
+
+    const trigger = new Trigger(
+      await getTriggerConstructorParams({
+        options: {},
+        name: key,
+      })
+    );
+    const run = trigger.run;
+    const webhooks = trigger.webhooks;
+    expect(!!run || !!webhooks).toBe(true);
+  }
+});
